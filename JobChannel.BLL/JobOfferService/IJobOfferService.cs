9c981e002242ef4f9c196067eb5d89e80f3732d4
SamuelChapel <Samuel.Chapel@2isa.org>
2022-12-10 11:27:45 +0100

    Add DbSession, Folder structure, Connection string

---
+++ JobChannel.BLL/JobOfferService/IJobOfferService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Threading.Tasks;
+
+namespace JobChannel.BLL.JobOfferService
+{
+    public interface IJobOfferService
+    {
+
+    }
+}
---
+++ JobChannel.DAL/UOW/DbSession.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Common;
+using System.Data.SqlClient;
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace JobChannel.DAL.UOW
+{
+    public class DbSession : IDbSession
+    {
+        public DbConnection Connection { get; private set; }
+
+        public DbSession(IConfiguration configuration)
+        {
+            Connection = new SqlConnection
+            {
+                ConnectionString = configuration.GetConnectionString("OVH")
+            };
+
+            Connection.Open();
+        }
+
+        public void Dispose() => GC.SuppressFinalize(this);
+    }
+}
