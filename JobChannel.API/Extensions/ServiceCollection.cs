1c2bd0d0e16dadd1c2a73a096ee974b776dcfb8d
SamuelCDA6 <Samuel.Chapel@2isa.org>
2022-12-12 14:24:27 +0100

    Add Contract GetAll

--- JobChannel.API/Extensions/ServiceCollection.cs
+++ JobChannel.API/Extensions/ServiceCollection.cs
@@ -1,4 +1,5 @@
-﻿using JobChannel.BLL.JobOfferService;
+﻿using JobChannel.BLL.ContractService;
+using JobChannel.BLL.JobOfferService;
 using JobChannel.DAL.UOW;
 using JobChannel.DAL.UOW.Repositories;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,6 +10,8 @@ namespace JobChannel.API.Extensions
     {
         public static IServiceCollection AddApiServices(this IServiceCollection services)
         {
+            services.AddScoped<IContractService, ContractService>();
+            services.AddScoped<IContractRepository, ContractRepository>();
             services.AddScoped<IJobOfferService, JobOfferService>();
             services.AddScoped<IJobOfferRepository, JobOfferRepository>();
             services.AddScoped<IDbSession, DbSession>();
---
+++ JobChannel.DAL/UOW/Repositories/ContractRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dapper;
+using JobChannel.Domain.BO;
+
+namespace JobChannel.DAL.UOW.Repositories
+{
+    public class ContractRepository : IContractRepository
+    {
+        private readonly IDbSession _dbSession;
+
+        public ContractRepository(IDbSession dbSession) => _dbSession = dbSession;
+
+        public async Task<IEnumerable<Contract>> GetAllContract()
+        {
+            string query = @"SELECT c.Id, c.Name, c.Code
+                            FROM JobChannel.Contract c";
+
+            return await _dbSession.Connection.QueryAsync<Contract>(query);
+        }
+    }
+}
