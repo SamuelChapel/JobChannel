a460400d9b2407362bdd36e413822a0a3af3eca6
SamuelCDA6 <Samuel.Chapel@2isa.org>
2022-12-14 11:54:07 +0100

    Modify Validations for JobOffer

--- JobChannel.Domain/DTO/JobOfferUpdateRequest.cs
+++ JobChannel.Domain/DTO/JobOfferUpdateRequest.cs
@@ -52,12 +52,12 @@ namespace JobChannel.Domain.DTO
     {
         public JobOfferUpdateRequestValidator()
         {
-            RuleFor(j => j.Id).NotNull().NotEmpty().GreaterThan(-1).WithMessage("");
+            RuleFor(j => j.Id).NotNull().NotEmpty().GreaterThan(-1).WithErrorCode("");
             RuleFor(j => j.Title).NotNull().NotEmpty().MaximumLength(200);
             RuleFor(j => j.Description).NotNull().NotEmpty().MaximumLength(8000);
-            RuleFor(j => j.PublicationDate).ExclusiveBetween(DateTime.Now, DateTime.Now.AddMonths(-6));
-            RuleFor(j => j.ModificationDate).GreaterThan(j => j.PublicationDate);
-            RuleFor(j => j.Url).NotNull().NotEmpty().Matches(@"https ?:\/\/ (www\.)?[-a - zA - Z0 - 9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)");
+            RuleFor(j => j.PublicationDate).ExclusiveBetween(DateTime.Now.AddMonths(-6), DateTime.Now);
+            RuleFor(j => j.ModificationDate).GreaterThanOrEqualTo(j => j.PublicationDate);
+            RuleFor(j => j.Url).NotNull().NotEmpty().Matches("https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)");
             RuleFor(j => j.Salary).NotNull().NotEmpty().MaximumLength(200);
             RuleFor(j => j.Experience).NotNull().NotEmpty().MaximumLength(100);
             RuleFor(j => j.Company).NotNull().NotEmpty().MaximumLength(100);
