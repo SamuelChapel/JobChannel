1d118568bd6af9277e2301535db6975de89e4e54
SamuelCDA6 <Samuel.Chapel@2isa.org>
2023-01-18 17:35:51 +0100

    Add Documentation with DocFx

---
+++ JobChannel.Doc/DocFxExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
+
+namespace JobChannel.Doc
+{
+    public class ConfigDocFxUI
+    {
+        public string Path { get; set; }
+    }
+
+    public static class DocFxExtension
+    {
+        public static void UseDocFxUI(this IApplicationBuilder app, Action<ConfigDocFxUI> settings)
+        {
+            ConfigDocFxUI configDocFxUI = new ConfigDocFxUI();
+
+            settings.Invoke(configDocFxUI);
+
+            configDocFxUI.Path ??= "/doc";
+
+            app.UseFileServer(new FileServerOptions()
+            {
+                RequestPath = new PathString(configDocFxUI.Path),
+                FileProvider = new EmbeddedFileProvider(Assembly.GetExecutingAssembly(), "JobChannel.DocFx._site")
+            });
+        }
+    }
+}
