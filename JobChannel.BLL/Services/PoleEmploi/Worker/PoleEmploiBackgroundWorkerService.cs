69431b798f90383e156a94f369a9bc3218d2a912
SamuelCDA6 <Samuel.Chapel@2isa.org>
2023-02-07 09:46:35 +0100

    Update Pole Emploi background worker
     - modify the scheduled time of the worker to get the job offers every days at midnight

--- JobChannel.BLL/Services/PoleEmploi/Worker/PoleEmploiBackgroundWorkerService.cs
+++ JobChannel.BLL/Services/PoleEmploi/Worker/PoleEmploiBackgroundWorkerService.cs
@@ -12,7 +12,7 @@ namespace JobChannel.BLL.Services.PoleEmploi.Worker
     {
         private readonly CrontabSchedule _schedule;
         private DateTime _nextRun;
-        private const string CronExpression = "8 9 * * *";
+        private const string CronExpression = "0 0 * * *";
 
         private readonly IServiceProvider _serviceProvider;
 
